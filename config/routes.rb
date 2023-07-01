03debcc11d50b4924f669cd7d99b3d16850d994f
youta777 <yousoccer07@yahoo.co.jp>
2023-07-02 02:46:49 +0900

    fix routes order

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
 
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
-  resources :users, only: [:index,:show,:edit,:update]
   devise_for :users
+
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :users, only: [:index,:show,:edit,:update]  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
